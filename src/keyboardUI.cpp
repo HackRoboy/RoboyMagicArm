5a9318c4b511b85c30274b3f03533c25e1c45183
Robin C. Petereit <robin.petereit@tum.de>
2017-10-21 21:21:52 +0200

    Add motor command class for ROS and add catkin configuration

---
+++ src/keyboardUI.cpp
@@ -0,0 +1,130 @@
+#include <stdio.h>
+#include <math.h>
+#include <ncurses.h>  //add in CMake!  Ref. https://answers.ros.org/question/69022/how-to-link-to-ncurses-library-in-the-make-file/
+#include "motorCommand.hpp"
+#include "ros/ros.h"
+#include <sstream>
+
+int main ()
+{
+    ROS_INFO("Magic arm node started");
+	MotorCommand motorCommand(std::string("magic_arm"));
+    ROS_INFO("Magic arm node initialized");
+
+    ROS_INFO("M1 (8)\tM2 (10)\tM3 (5)\tM4 (3)\tM5 (12)\tM6 (11)\n\t\t\tUP:\tA\tS\tD\tF\tG\tH\n\t\t\tDOWN:\tZ\tX\tC\tV\tB\tN\n");
+
+    int c; //user keyboard input
+    int motorAngle[6] = { 0 };  //int/float?!
+    int s, base; //"scale" = base ^ pow
+    s = 1;
+    base = 2;
+
+    do
+    {
+        c = getchar();   //need "Enter" to cont.
+        switch(c)
+        {
+            //---Controlling Motor1~6---
+            case 'A':
+            case 'a':
+                motorAngle[0] = motorAngle[0] + s;
+                break;
+            case 'Z':
+            case 'z':
+                motorAngle[0] = motorAngle[0] - s;
+                break;
+            case 'S':
+            case 's':
+                motorAngle[1] = motorAngle[1] + s;
+                break;
+            case 'X':
+            case 'x':
+                motorAngle[1] = motorAngle[1] - s;
+                break;
+            case 'D':
+            case 'd':
+                motorAngle[2] = motorAngle[2] + s;
+                break;
+            case 'C':
+            case 'c':
+                motorAngle[2] = motorAngle[2] - s;
+                break;
+            case 'F':
+            case 'f':
+                motorAngle[3] = motorAngle[3] + s;
+                break;
+            case 'V':
+            case 'v':
+                motorAngle[3] = motorAngle[3] - s;
+                break;
+            case 'G':
+            case 'g':
+                motorAngle[4] = motorAngle[4] + s;
+                break;
+            case 'B':
+            case 'b':
+                motorAngle[4] = motorAngle[4] - s;
+                break;
+            case 'H':
+            case 'h':
+                motorAngle[5] = motorAngle[5] + s;
+                break;
+            case 'N':
+            case 'n':
+                motorAngle[5] = motorAngle[5] - s;
+                break;
+            
+            //---For scaling----
+            case '0':
+                s = 1;
+                break;
+            case '1':
+                s = pow(base,1);
+                break;
+            case '2':
+                s = pow(base,2);
+                break;
+            case '3':
+                s = pow(base,3);
+                break;
+            case '4':
+                s = pow(base,4);
+                break;
+            case '5':
+                s = pow(base,5);
+                break;
+            case '6':
+                s = pow(base,6);
+                break;
+            case '7':
+                s = pow(base,7);
+                break;
+            case '8':
+                s = pow(base,8);
+                break;
+            case '9':
+                s = pow(base,9);
+                break;
+            default:
+            	continue;
+        }
+
+        std::stringstream motorSetPoints;
+        motorSetPoints << "motorAngle[1~6] = [";
+        for (int i = 0; i < 6; ++i)
+        {
+        	motorSetPoints << ' ' << motorAngle[i] ;
+        	motorCommand.setMotor(i, motorAngle[i]);
+        }
+        motorSetPoints << "]\n";
+
+        motorCommand.publishMotorCmd();
+
+        ROS_INFO("%s", motorSetPoints.str().c_str());
+
+
+    }while ((c!='q') && (c!='Q'));
+
+
+    return 0;
+}
---
+++ src/motorCommand.hpp
@@ -0,0 +1,42 @@
+#ifndef MOTORCOMMAND_HPP_
+#define MOTORCOMMAND_HPP_
+
+#include "ros/ros.h"
+#include <string>
+
+class MotorCommand
+{
+
+public:
+	MotorCommand(std::string nodeName);
+	virtual ~MotorCommand();
+
+	//// Set current set point of given motorId
+	void setMotor(int motorId, int motorSetPoint);
+
+	//// Returns current set point of given motorId
+	int getMotor(int motorId);
+
+	//// Publishes one set point message
+	void publishMotorCmd();
+
+	static const int c_motorCountUsed = 6;
+
+private:
+	//// Initializes ROS node and memory
+	void init(std::string nodeName);
+
+	int m_motorSetPoints[c_motorCountUsed]; //> Actual set points
+
+    ros::NodeHandlePtr m_nodeHandle; //> Handle for ROS node
+    ros::Publisher m_motorCommand; //> Motor Command Publisher
+    ros::ServiceClient m_motorControl; //> Motor Control Service Client
+    ros::ServiceClient m_emergencyStop; //> Motor Emergency Stop Service Client
+
+    bool m_isInitialized;
+
+	static const int c_motorCountTotal = 14;
+	static const int c_usedMotors[c_motorCountUsed];
+};
+
+#endif /* MOTORCOMMAND_HPP_ */
